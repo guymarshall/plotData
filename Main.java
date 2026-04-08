b561832d424e634da7a3780d754840b30f887170
Guy Marshall <75757034+guymarshall@users.noreply.github.com>
2026-04-08 19:02:42 +0100

    ADD: GUI popup window for viewing image after generation

--- Main.java
+++ Main.java
@@ -1,8 +1,19 @@
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.AccessDeniedException;
+import java.awt.BorderLayout;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.SwingUtilities;
 
 public class Main {
+    public static int addPadding(int input) {
+        return (int) (input * 1.2);
+    }
+
     public static void main(String[] args) {
         String filename = "data.csv";
         boolean useLogScale = false;
@@ -21,5 +32,24 @@ public class Main {
             e.printStackTrace();
             System.exit(1);
         }
+
+        SwingUtilities.invokeLater(() -> {
+            JFrame frame = new JFrame("Chart Viewer");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+            ImageIcon icon = new ImageIcon("output.png");
+            int imageWidth = icon.getIconWidth();
+            int imageHeight = icon.getIconHeight();
+
+            JLabel label = new JLabel(icon);
+
+            label.setHorizontalAlignment(JLabel.CENTER);
+
+            frame.getContentPane().add(new JScrollPane(label), BorderLayout.CENTER);
+
+            frame.setSize(addPadding(imageWidth), addPadding(imageHeight));
+            frame.setLocationRelativeTo(null);
+            frame.setVisible(true);
+        });
     }
 }
